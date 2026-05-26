9b62b03d55b35151b0d6c9d3e4f87c3a022dbd09
Huu Xuan <xuan071104@gmail.com>
2026-05-27 00:03:57 +0700

    add google login api

--- BackEnd/src/routes/user.routes.ts
+++ BackEnd/src/routes/user.routes.ts
@@ -1,72 +1,10 @@
 import { Router } from 'express';
-import { createUser, login } from '../controllers/user.controller';
+import { createUser, googleAuth, login } from '../controllers/user.controller';
 
 const router = Router();
 
-/**
- * @swagger
- * /api/users:
- *   post:
- *     summary: Tạo mới user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *                 example: admin123
- *               password:
- *                 type: string
- *                 example: 123456
- *     responses:
- *       201:
- *         description: Tạo user thành công
- *       400:
- *         description: Dữ liệu không hợp lệ
- *       500:
- *         description: Lỗi server
- */
 router.post('/', createUser);
-
-/**
- * @swagger
- * /api/users/login:
- *   post:
- *     summary: Đăng nhập user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *                 example: admin123
- *               password:
- *                 type: string
- *                 example: 123456
- *     responses:
- *       200:
- *         description: Đăng nhập thành công
- *       400:
- *         description: Dữ liệu không hợp lệ
- *       401:
- *         description: Tài khoản hoặc mật khẩu không chính xác
- *       500:
- *         description: Lỗi server
- */
+router.post('/google-auth', googleAuth);
 router.post('/login', login);
 
 export default router;
